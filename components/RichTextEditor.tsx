f2a6c3a80755ad52c00d6725fcb41377b02b0b1a
sinn357 <sinn357@naver.com>
2026-01-02 19:44:01 +0900

    fix: Remove prose class blocking list rendering

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -165,7 +165,7 @@ export default function RichTextEditor({
     },
     editorProps: {
       attributes: {
-        class: 'prose prose-lg max-w-none focus:outline-none min-h-[300px] p-4'
+        class: 'focus:outline-none min-h-[300px] p-4'
       }
     }
   });
